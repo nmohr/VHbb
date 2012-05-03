86dbfa34273292c1a81947fe344f1ff35dc58069
bortigno <>
2012-05-03 12:00:31 +0000

    addded 50To100 fit

--- test/compile.sh
+++ test/compile.sh
@@ -3,3 +3,4 @@ g++ -q -o make_histos_Pt50To100 SideBandAnalysis-Pt50To100/make_histos_sideband.
 #g++ -q -o cutFlow cutFlow.cxx -I $ROOTSYS/include -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
 #g++ -q -o fit_VHbb fit_VHbb.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
 g++ -q -o fit_sideband_pt100 SideBandAnalysis-Pt100/fit_sideband.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
+g++ -q -o fit_sideband_pt50To100 SideBandAnalysis-Pt50To100/fit_sideband.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
