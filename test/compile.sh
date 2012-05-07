08df9f8f21848522b5a8c5ed0a0d46bf3ff47824
bortigno <>
2012-05-07 10:00:22 +0000

    added systematics

--- test/compile.sh
+++ test/compile.sh
@@ -4,3 +4,5 @@ g++ -q -o make_histos_Pt50To100 SideBandAnalysis-Pt50To100/make_histos_sideband.
 #g++ -q -o fit_VHbb fit_VHbb.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
 g++ -q -o fit_sideband_pt100 SideBandAnalysis-Pt100/fit_sideband.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
 g++ -q -o fit_sideband_pt50To100 SideBandAnalysis-Pt50To100/fit_sideband.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
+g++ -q -o fit_sideband_syst_pt100 SideBandAnalysis-Pt100/fit_sideband_syst.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
+g++ -q -o fit_sideband_syst_pt50To100 SideBandAnalysis-Pt50To100/fit_sideband_syst.cxx -I $ROOTSYS/include -l RooFit -l RooFitCore -L $ROOTSYS/lib `root-config --glibs`
