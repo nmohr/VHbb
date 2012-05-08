f2d19092cc15524050e3bcfb51e4460d60020ea6
bortigno <>
2012-05-08 08:11:07 +0000

    does the fit and all the systematics and write important information in text files

---
+++ test/do_fit_and_syst.sh
@@ -0,0 +1,10 @@
+./fit_sideband_pt50To100
+
+./fit_sideband_syst_pt50To100 btag_up
+./fit_sideband_syst_pt50To100 btag_down
+./fit_sideband_syst_pt50To100 mistag_up
+./fit_sideband_syst_pt50To100 mistag_down
+./fit_sideband_syst_pt50To100 jer_up
+./fit_sideband_syst_pt50To100 jer_down
+./fit_sideband_syst_pt50To100 jec_up
+./fit_sideband_syst_pt50To100 jec_down
