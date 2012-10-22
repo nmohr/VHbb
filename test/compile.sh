b73363c90df943640f95e8747433ed4d5227d116
bortigno <>
2012-10-22 14:53:45 +0000

    experimental c++ enabled

--- test/compile.sh
+++ test/compile.sh
@@ -1,5 +1,5 @@
 #g++ -q -o make_histos_Pt100 SideBandAnalysis-Pt100/make_histos_sideband.cxx -I $ROOTSYS/include -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
-g++ -o make_histos_Pt50To100 SideBandAnalysis-Pt50To100/make_histos_sideband.cxx -I $ROOTSYS/include -lMathMore -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
+g++ -std=c++0x -o make_histos_Pt50To100 SideBandAnalysis-Pt50To100/make_histos_sideband.cxx -I $ROOTSYS/include -lMathMore -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
 #g++ -o make_histos_Basic Basic/make_histos.cxx -I $ROOTSYS/include -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
 #g++ -q -o make_histos_CnC-Pt50To100 CnC-Pt50To100/make_histos.cxx -I $ROOTSYS/include -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
 #g++ -o make_histos_CnC CnC/make_histos.cxx -I $ROOTSYS/include -I $ROOTSYS/include/TMVA -l TMVA -L $ROOTSYS/lib `root-config --glibs`
