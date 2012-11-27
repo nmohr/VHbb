f16df3053664387ccb1155723efe4590678541b4
peller <>
2012-11-27 10:51:03 +0000

    run on SE read and write

--- python/runAll.sh
+++ python/runAll.sh
@@ -22,16 +22,25 @@ fi
 
 #cd /shome/peller/CMSSW_5_2_4_patch4/src/
 # this doesnt work for me..?
-cd $CMSSW_BASE/src/
-source /swshare/psit3/etc/profile.d/cms_ui_env.sh
-export SCRAM_ARCH="slc5_amd64_gcc462"
-source $VO_CMS_SW_DIR/cmsset_default.sh
-eval `scramv1 runtime -sh`
+
+#cd $CMSSW_BASE/src/
+#source /swshare/psit3/etc/profile.d/cms_ui_env.sh
+#export SCRAM_ARCH="slc5_amd64_gcc462"
+#source $VO_CMS_SW_DIR/cmsset_default.sh
+#eval `scramv1 runtime -sh`
+#export LD_LIBRARY_PATH=/swshare/glite/d-cache/dcap/lib/:$LD_LIBRARY_PATH
+
+
+#source /swshare/psit3/etc/profile.d/cms_ui_env.sh
 export LD_LIBRARY_PATH=/swshare/glite/d-cache/dcap/lib/:$LD_LIBRARY_PATH
-#unset TMP
-#unset TMPDIR
+#export SCRAM_ARCH="slc5_amd64_gcc462"
+#source $VO_CMS_SW_DIR/cmsset_default.sh
+#eval `scramv1 runtime -sh`
+source /swshare/ROOT/root_v5.30.01_slc5_amd64/bin/thisroot.sh
 
 mkdir $TMPDIR
+
+printenv
 
 #Path where the script write_regression_systematic.py and evaluateMVA.py are stored
 #execute=$PWD/UserCode/VHbb/python/
@@ -51,6 +60,15 @@ print config.get('Directories','samplepath')
 EOF`
 echo $pathAna
 configFile=config$energy
+
+storagesamples=`python << EOF 
+import os
+from BetterConfigParser import BetterConfigParser
+config = BetterConfigParser()
+config.read('./pathConfig$energy')
+print config.get('Directories','storagesamples')
+EOF`
+
 
 MVAList=`python << EOF 
 import os
@@ -79,13 +97,13 @@ fi
 #Run the scripts
 
 if [ $task = "prep" ]; then
-    ./prepare_environment_with_config.py -I $pathAna -O $pathAna/env/ -C ${energy}samples_nosplit.cfg
+    ./prepare_environment_with_config.py -I $storagesamples -O $pathAna/env/ -C ${energy}samples_nosplit.cfg
 fi
 if [ $task = "sys" ]; then
     ./write_regression_systematics.py -P $pathAna/env/ -S $sample -C $configFile -C pathConfig$energy
 fi
 if [ $task = "eval" ]; then
-    ./evaluateMVA.py -P $pathAna/env/sys/ -D $MVAList -S $sample -U 0 -C ${configFile} -C pathConfig$energy
+    ./evaluateMVA.py -D $MVAList -S $sample -U 0 -C ${configFile} -C pathConfig$energy
 fi
 if [ $task = "syseval" ]; then
     ./write_regression_systematics.py -P $pathAna/env/ -S $sample -C $configFile -C pathConfig$energy
