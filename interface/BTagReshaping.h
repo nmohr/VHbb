745824664fb0ab47208c129ede0f2809367220c3
nmohr <>
2013-03-01 10:32:35 +0000

    New btagging SF

--- interface/BTagReshaping.h
+++ interface/BTagReshaping.h
@@ -128,13 +128,14 @@ class BTagShapeInterface
 {
  public:
   BTagShapeInterface(){}
-  BTagShapeInterface(const char * file, float scaleBC, float scaleL, bool use4points=false, float boundX=1.001, float boundY=1.001) : m_file(new TFile(file))
+  BTagShapeInterface(const char * file, float scaleBC, float scaleL, bool use4points=false, float boundX=1.001, float boundY=1.001,unsigned int maxbins=9999) : m_file(new TFile(file))
   {
     std::vector<EtaPtBin> binsBC;
     std::vector< std::vector<std::pair<float, float> > > cutsAndSFB;
     std::vector< std::vector<std::pair<float, float> > > cutsAndSFC;
     float charmFactor = 2. -1.0 ; //additional uncertainty for charm
-    for(unsigned int i=0;i<beff::bins;i++)
+    if(maxbins > beff::bins)  maxbins = beff::bins;
+    for(unsigned int i=0;i<maxbins;i++)
     {
       EtaPtBin bin(-2.5,2.5,beff::ptmin[i],beff::ptmax[i]);
       binsBC.push_back(bin);
@@ -206,23 +207,23 @@ class BTagShapeInterface
       std::vector<std::pair< float, float > > cutsAndSFbinC;
       std::vector<std::pair< float, float > > cutsAndSFbinB;
 
-      binsBC.push_back(EtaPtBin(-2.5,2.5,beff::ptmax[beff::bins-1],9e99));
-      float sft = beff::CSVT_SFb(beff::ptmax[beff::bins-1]);
-      sft+=scaleBC * beff::CSVT_SFb_error[beff::bins-1]*2;// add error
+      binsBC.push_back(EtaPtBin(-2.5,2.5,beff::ptmax[maxbins-1],9e99));
+      float sft = beff::CSVT_SFb(beff::ptmax[maxbins-1]);
+      sft+=scaleBC * beff::CSVT_SFb_error[maxbins-1]*2;// add error
       cutsAndSFbinB.push_back(std::pair<float, float>(0.898, sft));
-      sft+=scaleBC * beff::CSVT_SFb_error[beff::bins-1]*charmFactor; // charm additional error
+      sft+=scaleBC * beff::CSVT_SFb_error[maxbins-1]*charmFactor; // charm additional error
       cutsAndSFbinC.push_back(std::pair<float, float>(0.898, sft));
       
-      float sfm = beff::CSVM_SFb(beff::ptmax[beff::bins-1]);
-      sfm+=scaleBC * beff::CSVM_SFb_error[beff::bins-1]*2; // add error
+      float sfm = beff::CSVM_SFb(beff::ptmax[maxbins-1]);
+      sfm+=scaleBC * beff::CSVM_SFb_error[maxbins-1]*2; // add error
       cutsAndSFbinB.push_back(std::pair<float, float>(0.679,sfm));
-      sfm+=scaleBC * beff::CSVM_SFb_error[beff::bins-1]*charmFactor; // charm additional error
+      sfm+=scaleBC * beff::CSVM_SFb_error[maxbins-1]*charmFactor; // charm additional error
       cutsAndSFbinC.push_back(std::pair<float, float>(0.679,sfm));
       
-      float sfl = beff::CSVL_SFb(beff::ptmax[beff::bins-1]);
-      sfl+=scaleBC * beff::CSVL_SFb_error[beff::bins-1]*2; // add error
+      float sfl = beff::CSVL_SFb(beff::ptmax[maxbins-1]);
+      sfl+=scaleBC * beff::CSVL_SFb_error[maxbins-1]*2; // add error
       cutsAndSFbinB.push_back(std::pair<float, float>(0.244,sfl));
-      sfl+=scaleBC * beff::CSVL_SFb_error[beff::bins-1]*charmFactor; // charm additional error
+      sfl+=scaleBC * beff::CSVL_SFb_error[maxbins-1]*charmFactor; // charm additional error
       cutsAndSFbinC.push_back(std::pair<float, float>(0.244,sfl));
 
       //std::cout << "Lastbin SFs " << sfl << " " << sfm << " " << sft << std::endl;
@@ -251,7 +252,7 @@ class BTagShapeInterface
       EtaPtBin bin(etamin[j],etamax[j],ptmin[i],ptmax[i]);
       binsL.push_back(bin);
       std::vector<std::pair< float, float > > cutsAndSFbinL;
-      float sft = mistag_CSVT(bin.centerEta(),bin.centerPt(),scaleL*1.5);
+      float sft = mistag_CSVT(bin.centerEta(),bin.centerPt(),scaleL);
       cutsAndSFbinL.push_back(std::pair<float, float>(0.898, sft));
 
       float sfm = mistag_CSVM(bin.centerEta(),bin.centerPt(),scaleL);
--- interface/btag_payload_light.h
+++ interface/btag_payload_light.h
@@ -1,18 +1,137 @@
 
+float mistag_CSVL(float eta,float x, float scale) {
+  float mean=-1.,min=-1.,max=-1.;
+  if(eta > 0.0 && eta <= 0.5) //   if( Atagger == "CSVL" && sEtamin == "0.0" && sEtamax == "0.5")
+      { 
+        mean = ((1.04901+(0.00152181*x))+(-3.43568e-06*(x*x)))+(2.17219e-09*(x*(x*x)));
+        min = ((0.973773+(0.00103049*x))+(-2.2277e-06*(x*x)))+(1.37208e-09*(x*(x*x)));
+        max = ((1.12424+(0.00201136*x))+(-4.64021e-06*(x*x)))+(2.97219e-09*(x*(x*x)));
+      } 
+  if(eta > 0.5 && eta <= 1.0) //    if( Atagger == "CSVL" && sEtamin == "0.5" && sEtamax == "1.0")
+      {
+        mean = ((0.991915+(0.00172552*x))+(-3.92652e-06*(x*x)))+(2.56816e-09*(x*(x*x)));
+        min = ((0.921518+(0.00129098*x))+(-2.86488e-06*(x*x)))+(1.86022e-09*(x*(x*x)));
+        max = ((1.06231+(0.00215815*x))+(-4.9844e-06*(x*x)))+(3.27623e-09*(x*(x*x)));
+      } 
+  if(eta > 1.0 && eta <= 1.5) //    if( Atagger == "CSVL" && sEtamin == "1.0" && sEtamax == "1.5")
+      {
+        mean = ((0.962127+(0.00192796*x))+(-4.53385e-06*(x*x)))+(3.0605e-09*(x*(x*x)));
+        min = ((0.895419+(0.00153387*x))+(-3.48409e-06*(x*x)))+(2.30899e-09*(x*(x*x)));
+        max = ((1.02883+(0.00231985*x))+(-5.57924e-06*(x*x)))+(3.81235e-09*(x*(x*x)));
+      }
+  if(eta > 1.5 && eta <= 2.4) //    if( Atagger == "CSVL" && sEtamin == "1.5" && sEtamax == "2.4")
+      {
+        mean = ((1.06121+(0.000332747*x))+(-8.81201e-07*(x*x)))+(7.43896e-10*(x*(x*x)));
+        min = ((0.983607+(0.000196747*x))+(-3.98327e-07*(x*x)))+(2.95764e-10*(x*(x*x)));
+        max = ((1.1388+(0.000468418*x))+(-1.36341e-06*(x*x)))+(1.19256e-09*(x*(x*x)));
+      }
+ if(scale > 0)     return (mean + (max-mean)*scale);
+  if(scale< 0)     return (mean + (mean-min)*scale);
+   return mean; 
+}
 
+float mistag_CSVM(float eta,float x, float scale) {
+  float mean=-1.,min=-1.,max=-1.;
+  if(eta > 0.0 && eta <= 0.8) //  if( Atagger == "CSVM" && sEtamin == "0.0" && sEtamax == "0.8")
+      { 
+        mean = ((1.06238+(0.00198635*x))+(-4.89082e-06*(x*x)))+(3.29312e-09*(x*(x*x)));
+        min = ((0.972746+(0.00104424*x))+(-2.36081e-06*(x*x)))+(1.53438e-09*(x*(x*x)));
+        max = ((1.15201+(0.00292575*x))+(-7.41497e-06*(x*x)))+(5.0512e-09*(x*(x*x)));
+      }
+  if(eta > 0.8 && eta <= 1.6) //    if( Atagger == "CSVM" && sEtamin == "0.8" && sEtamax == "1.6")
+      {
+        mean = ((1.08048+(0.00110831*x))+(-2.96189e-06*(x*x)))+(2.16266e-09*(x*(x*x)));
+        min = ((0.9836+(0.000649761*x))+(-1.59773e-06*(x*x)))+(1.14324e-09*(x*(x*x)));
+        max = ((1.17735+(0.00156533*x))+(-4.32257e-06*(x*x)))+(3.18197e-09*(x*(x*x)));
+      } 
+  if(eta > 1.6 && eta <= 2.4) //    if( Atagger == "CSVM" && sEtamin == "1.6" && sEtamax == "2.4")
+      {
+        mean = ((1.09145+(0.000687171*x))+(-2.45054e-06*(x*x)))+(1.7844e-09*(x*(x*x)));
+        min = ((1.00616+(0.000358884*x))+(-1.23768e-06*(x*x)))+(6.86678e-10*(x*(x*x)));
+        max = ((1.17671+(0.0010147*x))+(-3.66269e-06*(x*x)))+(2.88425e-09*(x*(x*x)));
+      }
+ if(scale > 0)     return (mean + (max-mean)*scale);
+  if(scale< 0)     return (mean + (mean-min)*scale);
+   return mean; 
+}
+
+float mistag_CSVT(float eta,float x, float scale) {
+  float mean=-1.,min=-1.,max=-1.;
+  if(eta > 0.0 && eta <= 2.4) //    if( Atagger == "CSVT" && sEtamin == "0.0" && sEtamax == "2.4")
+      {
+        mean = ((1.01739+(0.00283619*x))+(-7.93013e-06*(x*x)))+(5.97491e-09*(x*(x*x)));
+        min = ((0.953587+(0.00124872*x))+(-3.97277e-06*(x*x)))+(3.23466e-09*(x*(x*x)));
+        max = ((1.08119+(0.00441909*x))+(-1.18764e-05*(x*x)))+(8.71372e-09*(x*(x*x)));
+      }
+  if(scale > 0)     return (mean + (max-mean)*scale);
+  if(scale< 0)     return (mean + (mean-min)*scale);
+   return mean; 
+}
+
+/*
+   if( Atagger == "CSVL" && sEtamin == "0.0" && sEtamax == "0.5")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((1.04901+(0.00152181*x))+(-3.43568e-06*(x*x)))+(2.17219e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.973773+(0.00103049*x))+(-2.2277e-06*(x*x)))+(1.37208e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.12424+(0.00201136*x))+(-4.64021e-06*(x*x)))+(2.97219e-09*(x*(x*x)))", 20.,ptmax);
+      }
+    if( Atagger == "CSVL" && sEtamin == "0.5" && sEtamax == "1.0")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((0.991915+(0.00172552*x))+(-3.92652e-06*(x*x)))+(2.56816e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.921518+(0.00129098*x))+(-2.86488e-06*(x*x)))+(1.86022e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.06231+(0.00215815*x))+(-4.9844e-06*(x*x)))+(3.27623e-09*(x*(x*x)))", 20.,ptmax);
+      }
+    if( Atagger == "CSVL" && sEtamin == "1.0" && sEtamax == "1.5")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((0.962127+(0.00192796*x))+(-4.53385e-06*(x*x)))+(3.0605e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.895419+(0.00153387*x))+(-3.48409e-06*(x*x)))+(2.30899e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.02883+(0.00231985*x))+(-5.57924e-06*(x*x)))+(3.81235e-09*(x*(x*x)))", 20.,ptmax);
+      }
+    if( Atagger == "CSVL" && sEtamin == "1.5" && sEtamax == "2.4")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((1.06121+(0.000332747*x))+(-8.81201e-07*(x*x)))+(7.43896e-10*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.983607+(0.000196747*x))+(-3.98327e-07*(x*x)))+(2.95764e-10*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.1388+(0.000468418*x))+(-1.36341e-06*(x*x)))+(1.19256e-09*(x*(x*x)))", 20.,ptmax);
+      }
+  if( Atagger == "CSVM" && sEtamin == "0.0" && sEtamax == "0.8")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((1.06238+(0.00198635*x))+(-4.89082e-06*(x*x)))+(3.29312e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.972746+(0.00104424*x))+(-2.36081e-06*(x*x)))+(1.53438e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.15201+(0.00292575*x))+(-7.41497e-06*(x*x)))+(5.0512e-09*(x*(x*x)))", 20.,ptmax);
+      }
+    if( Atagger == "CSVM" && sEtamin == "0.8" && sEtamax == "1.6")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((1.08048+(0.00110831*x))+(-2.96189e-06*(x*x)))+(2.16266e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.9836+(0.000649761*x))+(-1.59773e-06*(x*x)))+(1.14324e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.17735+(0.00156533*x))+(-4.32257e-06*(x*x)))+(3.18197e-09*(x*(x*x)))", 20.,ptmax);
+      }
+    if( Atagger == "CSVM" && sEtamin == "1.6" && sEtamax == "2.4")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((1.09145+(0.000687171*x))+(-2.45054e-06*(x*x)))+(1.7844e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((1.00616+(0.000358884*x))+(-1.23768e-06*(x*x)))+(6.86678e-10*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.17671+(0.0010147*x))+(-3.66269e-06*(x*x)))+(2.88425e-09*(x*(x*x)))", 20.,ptmax);
+      }    if( Atagger == "CSVT" && sEtamin == "0.0" && sEtamax == "2.4")
+      {
+	if( meanminmax == "mean" ) tmpSFl = new TF1("SFlight","((1.01739+(0.00283619*x))+(-7.93013e-06*(x*x)))+(5.97491e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "min" ) tmpSFl = new TF1("SFlightMin","((0.953587+(0.00124872*x))+(-3.97277e-06*(x*x)))+(3.23466e-09*(x*(x*x)))", 20.,ptmax);
+	if( meanminmax == "max" ) tmpSFl = new TF1("SFlightMax","((1.08119+(0.00441909*x))+(-1.18764e-05*(x*x)))+(8.71372e-09*(x*(x*x)))", 20.,ptmax);
+      }
+
+*/
+/*
 float mistag_CSVL(float eta,float x, float scale) {
   float mean=-1.,min=-1.,max=-1.;
   if(eta >= 0.0 && eta <= 0.5) { 
     mean = ((1.07536+(0.000175506*x))+(-8.63317e-07*(x*x)))+(3.27516e-10*(x*(x*x)));
     min = ((0.994425+(-8.66392e-05*x))+(-3.03813e-08*(x*x)))+(-3.52151e-10*(x*(x*x)));
     max = ((1.15628+(0.000437668*x))+(-1.69625e-06*(x*x)))+(1.00718e-09*(x*(x*x)));
-  }
+  }*/
 /*if(eta > 0.0 && eta <= 2.4) { 
     mean = ((1.0344+(0.000962994*x))+(-3.65392e-06*(x*x)))+(3.23525e-09*(x*(x*x)));
     min = ((0.956023+(0.000825106*x))+(-3.18828e-06*(x*x)))+(2.81787e-09*(x*(x*x)));
     max = ((1.11272+(0.00110104*x))+(-4.11956e-06*(x*x)))+(3.65263e-09*(x*(x*x)));
   }*/
-  if(eta > 0.5 && eta <= 1.0) { 
+/*  if(eta > 0.5 && eta <= 1.0) { 
     mean = ((1.07846+(0.00032458*x))+(-1.30258e-06*(x*x)))+(8.50608e-10*(x*(x*x)));
     min = ((0.998088+(6.94916e-05*x))+(-4.82731e-07*(x*x)))+(1.63506e-10*(x*(x*x)));
     max = ((1.15882+(0.000579711*x))+(-2.12243e-06*(x*x)))+(1.53771e-09*(x*(x*x)));
@@ -40,13 +159,13 @@ float mistag_CSVM(float eta,float x, float scale) {
     mean = ((1.06182+(0.000617034*x))+(-1.5732e-06*(x*x)))+(3.02909e-10*(x*(x*x)));
     min = ((0.972455+(7.51396e-06*x))+(4.91857e-07*(x*x)))+(-1.47661e-09*(x*(x*x)));
     max = ((1.15116+(0.00122657*x))+(-3.63826e-06*(x*x)))+(2.08242e-09*(x*(x*x)));
-  }
+  }*/
 /*  if(eta > 0.0 && eta <= 2.4) { 
     mean = ((1.04318+(0.000848162*x))+(-2.5795e-06*(x*x)))+(1.64156e-09*(x*(x*x)));
     min = ((0.962627+(0.000448344*x))+(-1.25579e-06*(x*x)))+(4.82283e-10*(x*(x*x)));
     max = ((1.12368+(0.00124806*x))+(-3.9032e-06*(x*x)))+(2.80083e-09*(x*(x*x)));
   }*/
-  if(eta > 0.8 && eta <= 1.6) { 
+/*  if(eta > 0.8 && eta <= 1.6) { 
     mean = ((1.111+(-9.64191e-06*x))+(1.80811e-07*(x*x)))+(-5.44868e-10*(x*(x*x)));
     min = ((1.02055+(-0.000378856*x))+(1.49029e-06*(x*x)))+(-1.74966e-09*(x*(x*x)));
     max = ((1.20146+(0.000359543*x))+(-1.12866e-06*(x*x)))+(6.59918e-10*(x*(x*x)));
@@ -73,4 +192,4 @@ float mistag_CSVT(float eta,float x, float scale) {
   if(scale< 0)     return (mean + (mean-min)*scale)*  (1.19275 + -0.00191042*x + 2.92205e-06*x*x);
   return mean *( 1.19275 + -0.00191042*x + 2.92205e-06*x*x);
   
-}
+}*/
